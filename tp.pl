aee26b578ff4880f3a4adbd825accbd9ddc5e4c4
EstebanC99 <estebancarignani99@gmail.com>
2023-09-30 17:56:41 -0300

    Cambios en JugueBot para simular un bot de chat

--- tp.pl
+++ tp.pl
@@ -1,10 +1,25 @@
-:- dynamic juguete/7.
+:- dynamic(tematica/3).
+:- dynamic(juguete/7).
 
 abrir_db:-
   retractall(juguete(_,_,_,_,_,_,_)),
   consult('./Base/Datos.txt').
   
 menu:-
+  mostrarLogo,
+  writeln('Hola, espero que andes muy bien. Soy JugueBot, tu asesor de compras.'),
+  sleep(2),
+  mostrarOpciones,
+  read(Opt),
+  Opt \= x,
+  abrir_db,
+  seleccionar(Opt),
+  writeln('Espero a ver sido de ayuda :D !!'),
+  sleep(2),
+  menu.
+menu.
+
+mostrarLogo:-
   writeln('       _                        ____        _   '),
   writeln('      | |                      |  _ \\      | |  '),
   writeln('      | |_   _  __ _ _   _  ___| |_) | ___ | |_ '),
@@ -13,109 +28,156 @@ menu:-
   writeln('  \\____/ \\__,_|\\__, |\\__,_|\\___|____/ \\___/ \\__|'),
   writeln('                __/ |                           '),
   writeln('               |___/   Tu asistente de ventas   '),
-  writeln(''),
-  writeln(' ---- Elija una opcion --------------------'),
-  writeln('| 1. Asesorar a un nuevo cliente           |'),
-  writeln('| 2. Buscar juguetes por tematicas         |'),
-  writeln('| 3. Buscar juguetes por palabra clave     |'),
-  writeln('| x. Salir                                 |'),
-  writeln(' ------------------------------------------'),
-  read(Opt),
-  Opt \= x,
-  abrir_db,
-  seleccionar(Opt),
-  menu.
-menu.
-
-leer([TematicaMinus|T]):- 
-  write('\nIngrese una tematica (doble corchete para finalizar el ingreso): '), 
-  read(Tematica),
-  Tematica \= [], 
-  string_lower(Tematica, TematicaMinus),
-  leer(T).
-leer([]).
-
-imprimirTematicas:-
-  writeln('\t- Musica'),
-  writeln('\t- Deportes'),
-  writeln('\t- Construccion y diseno'),
-  writeln('\t- Ciencia y experimentos'),
-  writeln('\t- Disfraces'),
-  writeln('\t- Arte y manualidades'),
-  writeln('\t- Cocina y comida'),
-  writeln('\t- Lectura y escritura'),
-  writeln('\t- Figuras de accion'),
-  writeln('\t- Naturaleza y animales'),
-  writeln('\t- Rompecabezas'),
-  writeln('\t- Juegos al aire libre'),
-  writeln('\t- Juegos de agua'),
-  writeln('\t- Juegos de mesa'),
-  writeln('\t- Coleccionismo').
+  writeln('').
+mostrarOpciones:-
+  writeln(' ---- En que puedo ayudarte hoy? --------------------'),
+  writeln('| 1. Ver juguetes para un regalo                     |'),
+  writeln('| 2. Describir un juguete                            |'),
+  writeln('| x. Salir                                           |'),
+  writeln(' ----------------------------------------------------').
 
 seleccionar(1):- 
-  write('ASESORAMIENTO PARA UN NUEVO CLIENTE: '),
-  write('\nIngrese la edad de la persona: '), read(Edad),
-  write('\nIngrese una tematica de interes de las siguientes: '),
-  imprimirTematicas,
-  leer(Tematicas),
-  write('\nIngrese el genero de la persona: '), read(Genero), string_lower(Genero, GeneroMinus),
-  write('\nIngrese el precio maximo del juguete: '), read(PrecioMax),
-  buscarJuguetes(Edad, GeneroMinus, Tematicas, PrecioMax, JugPosibles),
+  writeln('Perfecto, me encanta esta opcion.'),
+  sleep(1),
+  writeln('Para ayudarte, tendre que hacerte algunas preguntas primero. No te preocupes, no son muchas...'),
+  consultarTematicas(TematicasElegidas, 1),
+  subtract(TematicasElegidas, [[]], TematicasFiltradas),
+  writeln(TematicasFiltradas),
+  buscarJuguetes(_, _, TematicasFiltradas, _, JugPosibles),
   mostrarJuguetes(JugPosibles).
-
 seleccionar(2):-
-	writeln('Ingrese una tematica de interes de las siguientes: '),
-  imprimirTematicas,
-  leer(Tematicas),
-  buscarPorTematicas(Tematicas, JugPosibles),
+  write('Perfecto, necesitare que me digas una breve descripcion del juguete que buscas'), read(Descripcion),
+  split_string(Descripcion, ' ', ' ', DescripcionSeparada),
+  limpiarConectores(DescripcionSeparada, PalabrasClave),
+  buscarPorPalabra(PalabrasClave, JugPosibles),
+  writeln('Mmm... Se me ocurren estas opciones:'),
   mostrarJuguetes(JugPosibles).
 
-seleccionar(3):-
-  write('Ingrese el nombre del juguete o su marca: '),
-  read(Palabra), string_lower(Palabra, PalabraMinus),
-  buscarPorPalabra(PalabraMinus, JugPosibles),
-  mostrarJuguetes(JugPosibles).
 
-mostrarJuguetes([JugueteID | RestoIDs]):-
-  abrir_db,
-  retract(juguete(JugueteID, Descripcion, _, _, _, Tematicas, Precio)),
-  write(JugueteID), write(' - '), write(Descripcion), write(' $'), write(Precio), write(' - Tematicas: '), write(Tematicas),
-  writeln(''),
-  mostrarJuguetes(RestoIDs).
-mostrarJuguetes([]):- write('Ingrese cualquier entrada para volver al menu: '), read(_).
+%% ------- SECCION DE BUSQUEDA PRINCIPAL --------------------
+  %% Regla que realizar preguntas al usuario relacionadas con las tematicas para ir filtrando intereses
+  consultarTematicas([_|R], 4):- 
+    write('Te gustaria elegir mas tematicas para agregar? (s|n) '),
+    read(Respuesta),
+    Respuesta \= n,
+    imprimirTematicas,
+    leer(R),
+    writeln('Genial, esas son las preguntas por ahora...'),
+    writeln('Dejame ver que opciones tengo...'),
+    sleep(1).
+  consultarTematicas([], 4):-
+    writeln('Genial, esas son las preguntas por ahora...'),
+    writeln('Dejame ver que opciones tengo...'),
+    sleep(1).
+  consultarTematicas([H|R], CantPreguntas):-
+    random(1, 16, TematicaID),
+    retract(tematica(TematicaID, Tematica, Articulo)),
+    realizarPregunta(CantPreguntas), write(Articulo), write(' '), write(Tematica), write('? (s/n) '),
+    read(Respuesta),
+    evaluarRespuestaSiNo(Respuesta, TematicaID, H),
+    Contador is CantPreguntas + 1,
+    consultarTematicas(R, Contador).
+  
+  %% Regla que evalúa que pregunta realizar en funcion de la cantidad de preguntas realizadas
+  realizarPregunta(1):-
+    write('\t > Le gusta\\n ').
+  realizarPregunta(2):-
+    write('\t > Crees que pueda interesarse en ').
+  realizarPregunta(3):-
+    write('\t > Sabes si disfruta de ').
 
-buscarJuguetes(Edad, Genero, TematicasInteres, Pmax, [JugueteID | Resto]):-
-  retract(juguete(JugueteID, _, EdadMin, EdadMax, GeneroJuguete, Tematicas, Precio)),
-  tematicasCoinciden(Tematicas, TematicasInteres),
-  evaluarGeneroJuguete(Genero, GeneroJuguete),
-  EdadMin =< Edad,
-  EdadMax >= Edad,
-  Pmax >= Precio,
-  buscarJuguetes(Edad, Genero, TematicasInteres, Pmax, Resto).
-buscarJuguetes(_, _, _, _, []).
+  %% Regla que valida que la respuesta sea 's' o 'n' para agregar o no la tematica a las de interes  
+  evaluarRespuestaSiNo(s, TematicaID, TematicaID):- 
+    writeln(''),
+    writeln('Genial, lo tendre en cuenta.').
+  evaluarRespuestaSiNo(n, _, []):- 
+    writeln(''),
+    writeln('Que pena, lo tendre en cuenta al recomendarte').
+  evaluarRespuestaSiNo(_, TematicaID, H):-
+    writeln(''),
+    write('Lo siento no pude entenderte...'),
+    read(Respuesta),
+    evaluarRespuestaSiNo(Respuesta, TematicaID, H).
 
-buscarPorPalabra(Palabra, [JugueteID | Resto]):-
-  retract(juguete(JugueteID, Descripcion, _, _, _, _, _)),
-  string_lower(Descripcion, DescripcionMinus),
-  sub_atom(DescripcionMinus, _, _, _, Palabra),
-  buscarPorPalabra(Palabra, Resto).
-buscarPorPalabra(_, []).
+  %% Regla de busqueda de juguetes segun respuestas principales
+  buscarJuguetes(_, _, TematicasInteres, _, [JugueteID | Resto]):-
+    retract(juguete(JugueteID, _, _, _, _, Tematicas, _)),
+    tematicasCoinciden(Tematicas, TematicasInteres),
+    % evaluarGeneroJuguete(Genero, GeneroJuguete),
+    % EdadMin =< Edad,
+    % EdadMax >= Edad,
+    % Pmax >= Precio,
+    buscarJuguetes(_, _, TematicasInteres, _, Resto).
+  buscarJuguetes(_, _, _, _, []).
 
-buscarPorTematicas(TematicasInteres, [JugueteID | Resto]):-
-  retract(juguete(JugueteID, _, _, _, _, Tematicas, _)),
-  tematicasCoinciden(Tematicas, TematicasInteres),
-  buscarPorTematicas(TematicasInteres, Resto).
-buscarPorTematicas(_, []).
+  %% Regla que evalúa que el género del juguete sea el ingresado o sea 'n' de no aplica
+  evaluarGeneroJuguete(Genero, Genero).
+  evaluarGeneroJuguete(_, n).
+  
+  %% Regla que valida si alguna de las temáticas del juguete coincide con alguna de las elegidas
+  tematicasCoinciden(_, []).
+  tematicasCoinciden([Tematica | _], TematicasInteres):-
+    pertenece(Tematica, TematicasInteres).
+  tematicasCoinciden([_ | Resto], TematicasInteres):-
+    tematicasCoinciden(Resto, TematicasInteres).
+%% ------- FIN SECCION DE BUSQUEDA PRINCIPAL --------------------
 
-tematicasCoinciden(_, []).
-tematicasCoinciden([Tematica | _], TematicasInteres):-
-  string_lower(Tematica, TematicaMinus),
-  pertenece(TematicaMinus, TematicasInteres).
-tematicasCoinciden([_ | Resto], TematicasInteres):-
-  tematicasCoinciden(Resto, TematicasInteres).
+%% ------- SECCION DE BUSQUEDA POR PALABRAS ---------------
+  %% Regla que recibe una lista de palabras claves ingresadas y busca que existan en la descripcion del juguete
+  buscarPorPalabra(Palabras, [JugueteID | Resto]):-
+    retract(juguete(JugueteID, Descripcion, _, _, _, _, _)),
+    compararDescripcion(Palabras, Descripcion),
+    buscarPorPalabra(Palabras, Resto).
+  buscarPorPalabra(_, []).
 
-pertenece(X, [X | _]).
-pertenece(X, [_ | Resto]):- pertenece(X, Resto).
+  %% Regla que compara cada palabra clave con las de la descripcion
+  compararDescripcion([Palabra|[]], Descripcion):-
+    string_lower(Descripcion, DescripcionLower),
+    split_string(DescripcionLower, ' ', ' ', DescripcionLista),
+    pertenece(Palabra, DescripcionLista).
+  compararDescripcion([Palabra|R], Descripcion):-
+    string_lower(Descripcion, DescripcionLower),
+    split_string(DescripcionLower, ' ', ' ', DescripcionLista),
+    pertenece(Palabra, DescripcionLista),
+    compararDescripcion(R, Descripcion).
 
-evaluarGeneroJuguete(Genero, Genero).
-evaluarGeneroJuguete(_, n).
+  %% Regla que elimina los conectores 'de', 'la', 'con', 'para' de la descripcion ingresada por el usuario
+  limpiarConectores(ListaEntrada, ListaFinal):-
+    string_lower(de, LowDe), string_lower(con, LowCon), string_lower(la, LowLa), string_lower(para, LowPara), 
+    subtract(ListaEntrada, [LowDe,LowCon,LowLa,LowPara], ListaFinal).
+%% ------- FIN SECCION DE BUSQUEDA POR PALABRAS ---------------
+
+%% ------- SECCION DE REGLAS COMUNES ---------------
+  %% Regla que muestra un listado de los juguetes posibles para el usuario
+  mostrarJuguetes([JugueteID | RestoIDs]):-
+    abrir_db,
+    retract(juguete(JugueteID, Descripcion, _, _, _, Tematicas, Precio)),
+    write('\t'), write(JugueteID), write(' -> '), write(Descripcion), write(' $'), write(Precio), write(' - Tematicas: '), write(Tematicas),
+    writeln(''),
+    mostrarJuguetes(RestoIDs).
+  mostrarJuguetes([]):-
+    writeln('--> Estas son todas las opciones que tengo por ahora D:'),
+    writeln(''),
+    sleep(1),
+    write('(Ingrese cualquier entrada para volver al menu)'), read(_).
+
+  %% Regla que carga las tematicas ingresadas en una lista de tematicas
+  leer([TematicaID|T]):- 
+    write('\nIngrese el numero de una tematica (x para finalizar): '), 
+    read(TematicaID),
+    TematicaID \= x,
+    leer(T).
+  leer([]).
+
+  %% Regla que evalua la pertenencia del elemento en una lista
+  pertenece(X, [X | _]).
+  pertenece(X, [_ | Resto]):- pertenece(X, Resto).
+
+  %% Regla que muestra un listado de las tematicas 
+  imprimirTematicas:-
+    retract(tematica(TematicaID, Tematica, _)),
+    string_upper(Tematica, TematicaUpper),
+    write('\t'), write(TematicaID), write('- '), write(TematicaUpper), writeln(''),
+    imprimirTematicas.
+  imprimirTematicas.
+%% ------- FIN SECCION DE REGLAS COMUNES ---------------
